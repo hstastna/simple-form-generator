71a8451dabd4cc2ea2c9fd886d1b1eaa5f3fb253
Hilda Stastna <stastna.hilda@proton.me>
2025-03-21 20:14:35 +0100

    Move header with fox image out of FormProvider
    Header of the page was part of FormContext.Provider unnecessary. Hence
    move it out.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,28 +35,28 @@ const Home: FC = () => {
   };
 
   return (
-    <FormProvider>
-      <main className="focus:outline-none">
-        <header className="flex justify-between items-center">
-          <div>
-            <h1 className="text-3xl font-bold bg-gradient-to-r from-green-400 to-lime-500 bg-clip-text text-transparent">
-              Simple Form Generator
-            </h1>
-            <p className="text-gray-600">
-              Build forms from JSON configuration for your React project!
-            </p>
-          </div>
+    <main className="focus:outline-none">
+      <header className="flex justify-between items-center">
+        <div>
+          <h1 className="text-3xl font-bold bg-gradient-to-r from-green-400 to-lime-500 bg-clip-text text-transparent">
+            Simple Form Generator
+          </h1>
+          <p className="text-gray-600">
+            Build forms from JSON configuration for your React project!
+          </p>
+        </div>
 
-          <Image
-            src="/fox-image.png"
-            alt="Fox mascot for the form generator"
-            className="rounded-full ml-2"
-            width={200}
-            height={200}
-            sizes="(max-width: 400px) 150px, 200px"
-          />
-        </header>
+        <Image
+          src="/fox-image.png"
+          alt="Fox mascot for the form generator"
+          className="rounded-full ml-2"
+          width={200}
+          height={200}
+          sizes="(max-width: 400px) 150px, 200px"
+        />
+      </header>
 
+      <FormProvider>
         <div className="mb-6 border-b border-gray-200">
           <nav
             className="flex space-x-6"
@@ -89,8 +89,8 @@ const Home: FC = () => {
           {activeTab === 'config' && <ConfigTab />}
           {activeTab === 'result' && <ResultTab />}
         </section>
-      </main>
-    </FormProvider>
+      </FormProvider>
+    </main>
   );
 };
 
