dfcba242ba654561c2a0d9ffa4ef3e5459330c20
Hilda Stastna <hilda.stastna@mibcon.cz>
2026-09-06 17:11:55 +0200

    fix: use red-600 for the asterisk in RequiredMark component, add a dark-mode variant

--- src/components/tabs/ResultTab/components/RequiredMark.tsx
+++ src/components/tabs/ResultTab/components/RequiredMark.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 export const RequiredMark: FC = () => (
   <>
-    <span className="text-red-500 ml-1" aria-hidden="true">
+    <span className="ml-1 text-red-600 dark:text-red-400" aria-hidden="true">
       *
     </span>
     <span className="sr-only"> (required)</span>
